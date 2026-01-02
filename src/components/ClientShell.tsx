30028cddba4837225a95e9fdbdbabb6edb68e0b6
wang6388-ux <wang6388@uw.edu>
2026-01-01 22:09:22 -0800

    Update ClientShell.tsx

--- src/components/ClientShell.tsx
+++ src/components/ClientShell.tsx
@@ -6,7 +6,6 @@ import AppShell from "@/components/AppShell";
 export default function ClientShell({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  // ✅ 你可以把需要全屏的页面路径都加进来
   const fullScreen = pathname === "/dropoff";
 
   return <AppShell fullScreen={fullScreen}>{children}</AppShell>;
